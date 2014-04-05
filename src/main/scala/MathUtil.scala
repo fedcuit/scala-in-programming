fdf1578228542b59538725df4aeb8f45e315230d
Erdong Feng <edfeng@thoughtworks.com>
2014-04-05 19:51:05 +0800

    implement multiplication table in functional style.

---
+++ src/main/scala/MathUtil.scala
@@ -0,0 +1,13 @@
+object MathUtil {
+
+  def makeRow(i: Int): String = {
+    val rowSeq = for (j <- 1 to 10) yield StringUtils.paddingRight((i * j).toString, 4)
+    rowSeq.mkString
+  }
+
+  def multiTable(): String = {
+    val table = for (i <- 1 to 10) yield makeRow(i)
+    table.mkString("\n")
+  }
+
+}
