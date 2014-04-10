9b98c45b6070368dde9b8985d652e425606b51b0
Erdong Feng <edfeng@thoughtworks.com>
2014-04-10 13:41:07 +0800

    using zip.

--- src/main/scala/oo/ArrayElement.scala
+++ src/main/scala/oo/ArrayElement.scala
@@ -2,7 +2,11 @@ package oo
 
 class ArrayElement(override val contents: Array[String]) extends Element {
   override def beside(e: Element): Element = {
-    new ArrayElement(for ((line1, line2) <- contents zip e.contents) yield line1 + line2)
+    new ArrayElement(
+      for (
+        (line1, line2) <- contents zip e.contents
+      ) yield line1 + line2
+    )
   }
 
   override def above(e: Element): Element = new ArrayElement(this.contents ++ e.contents)
