097a248656e352d973f722616c8ea55ffa25a16c
Erdong Feng <edfeng@thoughtworks.com>
2014-05-10 22:58:47 +0800

    try with some methods on List.

---
+++ src/main/scala/collection/ListDemo.scala
@@ -0,0 +1,9 @@
+package collection
+
+object ListDemo {
+  def multiTable: String = {
+    List.tabulate(9, 9)((r, c) => (r + 1) * (c + 1)).map(_.mkString(" ")).mkString("\n")
+  }
+
+  def reverseLeft[T](xs: List[T]) = (List[T]() /: xs) { (ys, y) => y :: ys}
+}
---
+++ src/test/scala/collection/ListDemo$Test.scala
@@ -0,0 +1,15 @@
+package collection
+
+import org.scalatest.{ShouldMatchers, FunSpec}
+
+class ListDemo$Test extends FunSpec with ShouldMatchers {
+  describe("List") {
+    it("should support reverse itself using leftFold") {
+      ListDemo.reverseLeft(List(1, 2, 3)) should equal(List(3, 2, 1))
+    }
+
+    it("should able to create a multiplication table with tabulate") {
+      println(ListDemo.multiTable)
+    }
+  }
+}
